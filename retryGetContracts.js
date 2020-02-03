a697136c918255aa577473fd7ba15a0ae4fe288f
leekt216 <leekt216@gmail.com>
2020-02-03 23:06:26 +0900

    temp

--- retryGetContracts.js
+++ retryGetContracts.js
@@ -1,4 +1,5 @@
 //https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${address}
+
 const axios = require('axios');
 var fs = require('fs'); 
 const csv = require('fast-csv');
@@ -50,21 +51,21 @@ function saveResult() {
 }
 
 async function download(){
-  const stream = fs.createReadStream(path.resolve(__dirname, 'addresses.csv'))
+  const stream = fs.createReadStream(path.resolve(__dirname, 'addresses_filtered.csv'))
   //.pipe(csv.parse({ headers: true }))
   csv.parseStream(stream, {headers: true})
   .on('error', error => console.error(error))
   .on('data', (row) => {
     status.push({
-      address : row.receipt_contract_address,
+      address : row.address,
       status : "pending"
     });
   })
   .on('end', async (rowCount) =>{
     console.log(`Parsed ${rowCount} rows`);
-    for(var i=0; i<=status.length/1000; i++){
-      const sub_status = status.slice(i*1000, (i+1)*1000);
-      console.log(`Downloading ${i*1000} ~ ${(i+1)*1000 - 1}`)
+    for(var i=0; i<=status.length/100; i++){
+      const sub_status = status.slice(i*100, (i+1)*100);
+      console.log(`Downloading ${i*100} ~ ${(i+1)*100 - 1}`)
       const pr = sub_status.map((x,index) => {
         return isVerified(x.address).
           catch(function (err) {
@@ -80,6 +81,7 @@ async function download(){
               ]
             }
             else if (res.status == false) {
+              console.log(`${x.address} does not exist on etherscan`);
               return [
                 x.address,
                 "N/A"
